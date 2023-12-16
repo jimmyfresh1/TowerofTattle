66fcab1c3fabe5a8c75d19d3c98302df690b07f7
jimmyfresh1 <jamesfern14@gmail.com>
2023-12-16 11:55:05 -0600

    added add ps functionality

--- work/Command/Beats/script.js
+++ work/Command/Beats/script.js
@@ -1,4 +1,19 @@
-const paragraphs = document.querySelectorAll('#main-text p');
-paragraphs.forEach((p) => {
-    p.addEventListener('mouseover', function() {p.style.cursor= 'url(./images/pencil_edit.png), auto';})
-})
+const paragraphs = document.querySelectorAll('#main-text p').forEach (notebookLine => {
+    let addButton=document.createElement('button');
+    addButton.className='add-button'
+    let addIcon= document.createElement('img'); 
+    addIcon.src='./images/add.png';
+    addButton.appendChild(addIcon);
+    notebookLine.appendChild(addButton)
+});
+
+document.querySelectorAll('p .add-button').forEach(addButton => {
+    addButton.onclick = 
+    // function () {console.log("Yes, it blends!")} test
+    function () {
+    let newLine = document.createElement('p');
+    newLine.setAttribute('contenteditable', 'true');
+    newLine.textContent = 'Click to Edit';
+    addButton.parentNode.insertAdjacentElement('afterend', newLine)
+} 
+});
